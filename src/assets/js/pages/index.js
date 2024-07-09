19810c1d7c4500923909b5f1a472bde53fd71445
aisteron <timotheus@list.ru>
2024-07-09 22:11:20 +0300

    w09.07

--- src/assets/js/pages/index.js
+++ src/assets/js/pages/index.js
@@ -4,6 +4,12 @@ export function Pages(){
 	
 	// слайдер на главной
 	index_page_swiper()
+
+	// слайдер на странице продукта
+	prod_page_swiper()
+
+	// открытие / закрытие формы заказа
+	dialog_open_close()
 }
 
 function index_page_swiper(){
@@ -32,4 +38,46 @@ function index_page_swiper(){
 		new Swiper(iswiper, options)
 	})
 	
+}
+
+function prod_page_swiper(){
+	const iswiper = qs('.prod.swiper')
+	if(!iswiper) return
+
+	document.listen('swiper_loaded', async e => {
+		
+		await new Promise(resolve => setTimeout(()=>resolve(), 2000))
+		
+		qsa('.prod.swiper .swiper-slide.hidden').forEach(el=> el.classList.remove('hidden'))
+		
+		const options = {
+			spaceBetween: 30,
+			navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+			pagination: {
+        el: ".swiper-pagination",
+      }
+			
+		}
+		new Swiper(iswiper, options)
+	})
+}
+
+function dialog_open_close(){
+	const arr = []
+	const dialog = qs('dialog#formOrder')
+	
+	qs('.index-page') && arr.push(qs('.content button.Primary'))
+	qs('.prod-page') && arr.push(qs('.row .Primary'))
+
+	arr.forEach(el => {
+		el.listen("click", e => {
+			dialog.showModal()
+		})
+	})
+
+	// close
+	qs('.close', dialog).listen("click", e => dialog.close())
 }
