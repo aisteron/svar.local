44211dcfac007e1cf31f1c730b05addfe963324b
aisteron <timotheus@list.ru>
2024-07-11 15:27:24 +0300

    w11.07

--- src/assets/js/pages/index.js
+++ src/assets/js/pages/index.js
@@ -1,4 +1,4 @@
-import { qs, qsa } from "../libs"
+import { load_toast, qs, qsa } from "../libs"
 
 export function Pages(){
 	
@@ -10,6 +10,9 @@ export function Pages(){
 
 	// открытие / закрытие формы заказа
 	dialog_open_close()
+
+	// отправка формы
+	dialog_form_send()
 }
 
 function index_page_swiper(){
@@ -46,7 +49,7 @@ function prod_page_swiper(){
 
 	document.listen('swiper_loaded', async e => {
 		
-		await new Promise(resolve => setTimeout(()=>resolve(), 2000))
+		await new Promise(resolve => setTimeout(()=>resolve(), 100))
 		
 		qsa('.prod.swiper .swiper-slide.hidden').forEach(el=> el.classList.remove('hidden'))
 		
@@ -82,4 +85,65 @@ function dialog_open_close(){
 
 	// close
 	qs('.close', dialog).listen("click", e => dialog.close())
+}
+
+function dialog_form_send(){
+	let form = qs('#formOrder form')
+
+	// prefill form on dev mode 
+	if(process.env.NODE_ENV == 'development'){
+		qs('[name="name"]',form).value = 'name'
+		qs('[name="phone"]',form).value = '666'
+		qs('[name="email"]',form).value = 'test@email.net'
+		qs('[name="message"]',form).value = 'message from textarea'
+	}
+
+	
+	
+	form?.listen("submit",async e => {
+		e.preventDefault()
+
+		let obj = {
+			name: qs('[name="name"]',form).value,
+			phone: qs('[name="phone"]',form).value,
+			email: qs('[name="email"]',form).value,
+			message: qs('[name="message"]',form).value,
+			action: 'callback'
+		}
+		
+		let path = process.env.NODE_ENV == 'development' ? 'http://test.local/api.php' : '/api/'
+
+		let response = await fetch(path,{
+			method:'POST',
+			headers: { "Content-Type": "application/json"},
+			body: JSON.stringify(obj),
+		})
+
+		if(!response.ok){
+			await load_toast()
+			new Snackbar('Ошибка сервера');
+			return;
+		}
+
+		try {
+			response = await response.json()
+		} catch(e){
+			console.log(e)
+			await load_toast()
+			new Snackbar('Ошибка сервера. Чтение json');
+			return;
+
+		}
+
+		if(response.success){
+			await load_toast()
+			new Snackbar('Успешно отправлено!');
+			form.reset()
+		} else {
+			await load_toast()
+			new Snackbar(response.message || 'Ошибка сервера undefined');
+		}
+
+
+	})
 }
